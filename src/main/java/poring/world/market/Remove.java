e46cedf4ca52747da22d96aff4017a7d730bbacb
Edmilson Frank <edmilson.machado@adroll.com>
2020-04-10 22:10:28 -0300

    rework remove

--- src/main/java/poring/world/market/Remove.java
+++ src/main/java/poring/world/market/Remove.java
@@ -1,6 +1,8 @@
 package poring.world.market;
 
 import static poring.world.Constants.GLOBAL_CALL;
+import static poring.world.Constants.HELP;
+import static poring.world.Constants.LIST;
 
 import com.google.common.collect.ImmutableList;
 import org.javacord.api.entity.message.MessageAuthor;
@@ -21,12 +23,12 @@ public class Remove extends Command {
     String query = Utils.getQuery(command);
     List<WatchObject> objList = watcher.getMap().get(messageAuthor.getId());
     if (query == null || query.isEmpty()) {
-      event.getChannel().sendMessage("No index specified, _" + messageAuthor.getDisplayName() +
-          "_. Try _!" + GLOBAL_CALL + " help list_ for more information");
+      event.getChannel().sendMessage(
+          String.format("No index, Try _!%s %s %s_ for more information", GLOBAL_CALL, HELP, LIST));
       return;
     }
 
-    if (objList != null) {
+    if (objList != null && !objList.isEmpty()) {
       int pos = Integer.parseInt(query);
       if (pos > objList.size()) {
         event.getChannel().sendMessage(String.format("Maximum value to remove is **%s**", objList.size()));
@@ -35,12 +37,8 @@ public class Remove extends Command {
       WatchObject removed = objList.remove(pos - 1);
       watcher.saveMap();
 
-      String sb = "Removed _" +
-          removed.getQuery() +
-          "_ for _" +
-          messageAuthor.getDisplayName() +
-          "_\n";
-      event.getChannel().sendMessage(sb);
+      event.getChannel().sendMessage(
+          String.format("Removed _%s_ for _%s_", removed.getQuery(), messageAuthor.getDisplayName()));
     } else {
       event.getChannel().sendMessage("No watch list for _" + messageAuthor.getDisplayName() + "_");
     }
@@ -48,7 +46,8 @@ public class Remove extends Command {
 
   @Override
   public String getHelp() {
-    return "removes selected item from you watch list. try _!" + GLOBAL_CALL + " list_ to check the index before removing";
+    return "removes selected item from you watch list. try _!" + GLOBAL_CALL +
+        " list_ to check the index before removing";
   }
 
   @Override
