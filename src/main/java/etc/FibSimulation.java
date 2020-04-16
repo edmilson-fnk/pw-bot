ee5f7e94446e1d2aed7485c31d04f79e90132361
Edmilson Frank <edmilson.machado@adroll.com>
2020-04-16 13:40:40 -0300

    fib simulation

--- src/main/java/etc/FibSimulation.java
+++ src/main/java/etc/FibSimulation.java
@@ -1,4 +1,4 @@
-package calc;
+package etc;
 
 import java.text.DecimalFormat;
 
@@ -6,24 +6,24 @@ public class FibSimulation {
 
   public static void main(String[] args) {
     float aspd = 4.8f; // 480%
-    int hitNormal = 370;
+    int battleDur = 10;
+    int hitNormal = 360;
+    int hitFibAct = 460;
     int hitFib = 350;
-    int hitFibAct = 440;
-    float battleDur = 20f;
 
     simulate(aspd, hitNormal, hitFib, hitFibAct, battleDur);
   }
 
-  private static void simulate(float aspd, int hitNormal, int hitFib, int hitFibAct, float battleDur) {
-    int N = 5000;
+  private static void simulate(float aspd, int hitNormal, int hitFib, int hitFibAct, int battleDur) {
+    int N = 100000;
     float finalFibDmg = 0f;
     float finalDmg = 0f;
-    for (int n = 0; n < N; n++) {
-      int fibDur = 3;
-      float fibChance = 0.05f;
+    int fibDur = 3;
+    float fibChance = 0.05f;
+    float totalHits = aspd * battleDur;
 
+    for (int n = 0; n < N; n++) {
       int buffedHits = 0;
-      float totalHits = aspd * battleDur;
       float dmg = 0f;
       float fibDmg = 0f;
       for (int i = 0; i < totalHits; i++) {
@@ -49,6 +49,7 @@ public class FibSimulation {
     finalFibDmg /= N;
     System.out.println("Fib\t" + new DecimalFormat("###,###,###").format(finalFibDmg));
     System.out.println("Dmg\t" + new DecimalFormat("###,###,###").format(finalDmg));
+    System.out.println("Diff\t" + new DecimalFormat("###.##").format(100*(finalDmg-finalFibDmg)/finalDmg));
   }
 
   public static double rand(int min, int max) {
