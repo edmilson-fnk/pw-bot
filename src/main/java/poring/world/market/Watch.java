553bcc5edbe96741e0eb04727a5af9e5edec21a6
Edmilson Frank <edmilson.machado@adroll.com>
2020-05-14 13:39:51 -0300

    log

--- src/main/java/poring/world/market/Watch.java
+++ src/main/java/poring/world/market/Watch.java
@@ -30,7 +30,7 @@ public class Watch extends Command {
       for (int i = 1; i < queryFilters.length; i++) {
         String[] keyValue = queryFilters[i].split("=");
         String key = keyValue[0].trim();
-        String value = keyValue[1].trim();
+        String value = keyValue.length > 1 ? keyValue[1].trim() : "";
         for (String queryFilter : QUERY_FILTERS) {
           if (key.equalsIgnoreCase(queryFilter)) {
             String validate = FilterUtils.validate(key, value);
