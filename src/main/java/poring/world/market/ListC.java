7fb7bfd5f9da5291942436976537a217ab041b41
Edmilson Frank <edmilson.machado@adroll.com>
2020-05-18 16:52:43 -0300

    remove duplicate

--- src/main/java/poring/world/market/ListC.java
+++ src/main/java/poring/world/market/ListC.java
@@ -22,9 +22,6 @@ public class ListC extends Command {
     List<String> messages = new LinkedList<>();
     if (watcherMap.containsKey(messageAuthor.getId()) && !watcherMap.get(messageAuthor.getId()).isEmpty()) {
       StringBuilder sb = new StringBuilder();
-      sb.append("Items for **");
-      sb.append(messageAuthor.getDisplayName());
-      sb.append("**\n");
       sb.append(String.format("Items for **%s**\n", messageAuthor.getDisplayName()));
       List<WatchObject> objList = watcherMap.get(messageAuthor.getId());
       Map<String, Map<String, String>> filtersList = filtersMap != null ? filtersMap.get(messageAuthor.getId()) : null;
