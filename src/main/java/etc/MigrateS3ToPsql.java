774d4d91f5f8d675a11a11e9555b04366e2475a5
Edmilson Frank <edmilson.machado@adroll.com>
2020-05-27 22:24:34 -0300

    interface to class

--- src/main/java/etc/MigrateS3ToPsql.java
+++ src/main/java/etc/MigrateS3ToPsql.java
@@ -11,7 +11,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-public interface MigrateS3ToPsql {
+public class MigrateS3ToPsql {
 
   public static void main(String[] args) {
     Watcher watcher = new Watcher(null);
