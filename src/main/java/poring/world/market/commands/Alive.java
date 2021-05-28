d2f778de4af61c1c0dba265749c5226852cb7379
edmilson <edmilson.machado@nextroll.com>
2021-05-28 14:04:37 -0300

    custom reaction

--- src/main/java/poring/world/market/commands/Alive.java
+++ src/main/java/poring/world/market/commands/Alive.java
@@ -6,17 +6,30 @@ import org.javacord.api.event.message.MessageCreateEvent;
 import poring.world.market.Command;
 import poring.world.watcher.Watcher;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Alive extends Command {
+
+  private static final Map<Long, String> CUSTOM_REACTION = new HashMap<Long, String>(){{
+    this.put(775727348183466025L, "sqbobo:778345736806596628");
+  }};
+  public static final String DEFAULT_VALUE = ":handshake:";
+
   @Override
   public void run(String[] command, MessageCreateEvent event, Watcher watcher) {
-    event.getMessage().addReaction(EmojiParser.parseToUnicode(":handshake:"));
+    long userId = event.getMessageAuthor().getId();
+    if (CUSTOM_REACTION.containsKey(userId)) {
+      event.getMessage().addReaction(CUSTOM_REACTION.get(userId));
+    } else {
+      event.getMessage().addReaction(EmojiParser.parseToUnicode(DEFAULT_VALUE));
+    }
   }
 
   @Override
   public String getHelp() {
-    return "is GTB alive? Reacts :handshake: if so";
+    return "is GTB alive? Reacts if so";
   }
 
   @Override
